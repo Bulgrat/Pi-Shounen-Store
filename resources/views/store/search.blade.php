424012f725f39f1d3f586a94feadd0271afe8393
vJosph <viniciusjose38@hotmail.com>
2023-05-31 11:09:32 -0300

    sortBy name na home e search

--- resources/views/store/search.blade.php
+++ resources/views/store/search.blade.php
@@ -9,9 +9,12 @@
         </div>
     </div>
     <div class="row justify-content-center justify-content-lg-start">
-        @foreach ($products as $product)
+        @foreach ($products->sortBy('name') as $product)
         <div class="card bg-light mx-3 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
-            <img src="{{ asset($product->image) }}" class="card-img-top card-img-top h-100 w-100">
+            <a class="h-100 w-100" href="{{ route('show.product', $product->id) }}">
+
+                <img src="{{ asset($product->image) }}" class="card-img-top ">
+            </a>
             <h5 class="card-title text-center">{{ $product->name }}</h5>
 
             <div class="text-center">
@@ -19,11 +22,10 @@
                 <span class="text-muted">R$ {{ number_format($product->price, 2, ',', '.')}}</span>
             </div>
             <div class="card-body text-center">
-                <a style="background-color:#d22630" href="{{ route('show.product', $product->id) }}" class="btn btn-sm card-link text-white">Visualizar</a>
-
+                <a style="background-color:#101820" href="{{ route('show.product', $product->id) }}" class="btn btn-md card-link text-white">Visualizar</a>
                 <form action="{{ route('cart.store', $product->id) }}" method="POST" style="display:inline">
                     @csrf
-                    <button style="background-color:#101820" type="submit" class="btn text-white btn-sm card-link">Comprar</button>
+                    <button style="background-color:#d22630" type="submit" class="btn text-white btn-sm card-link">Comprar <img src="/public/images/cart2.png" width="25"></button>
                 </form>
             </div>
         </div>
